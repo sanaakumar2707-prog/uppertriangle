d4e4449f49eecb479d075c2c9ea6c7962cdc130e
sanaa <Sanaakumar07@gmail.com>
2025-09-22 10:13:27 +0530

    Initial Commit

---
+++ uppertriangle/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  uppertriangle
+//
+//  Created by Sanaa Kumar on 22/09/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
