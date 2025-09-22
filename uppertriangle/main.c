c634420fa6e5d0f18595226048797d7b6dce0f30
sanaa <Sanaakumar07@gmail.com>
2025-09-22 10:20:19 +0530

    uppertriangle

--- uppertriangle/main.c
+++ uppertriangle/main.c
@@ -1,15 +1,24 @@
-//
-//  main.c
-//  uppertriangle
-//
-//  Created by Sanaa Kumar on 22/09/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+    int matrix[3][3];
+        int sum = 0;
+
+        printf("Enter the elements of the 3x3 matrix:\n");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                scanf("%d", &matrix[i][j]);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j > i) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        printf("Sum of upper triangle elements: %d\n", sum);
+
+        return 0;
 }
